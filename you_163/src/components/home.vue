c67da985e27c63833edf66d036cd9a16539b36b9
RejoiceGrass <2604274012@qq.com>
2021-06-09 18:56:01 +0800

    master

--- you_163/src/components/home.vue
+++ you_163/src/components/home.vue
@@ -20,20 +20,24 @@
     <!-- Swipe -->
     <!-- 滚动的tab按钮列表 -->
     <van-tabs>
-      <van-tab v-for="(n,inx) in tabBtnList" :key="inx" :title="n">
-
+      <van-tab v-for="(n, inx) in tabBtnList" :key="inx" :title="n">
         <!-- 内容 -->
-        {{n}}
+        <!-- {{n}} -->
       </van-tab>
 
       <!-- 轮播图 -->
     </van-tabs>
     <van-swipe class="my-swipe" :autoplay="1000" indicator-color="white">
-      <van-swipe-item>1</van-swipe-item>
-      <van-swipe-item>2</van-swipe-item>
-      <van-swipe-item>3</van-swipe-item>
-      <van-swipe-item>4</van-swipe-item>
+      <van-swipe-item v-for="(url, inx) in imgObj" :key="inx" :title="url">
+        <img :src="url" />
+      </van-swipe-item>
     </van-swipe>
+    <!-- 滚动条下的广告-->
+    <van-row>
+      <van-col span="8">span: 8</van-col>
+      <van-col span="8">span: 8</van-col>
+      <van-col span="8">span: 8</van-col>
+    </van-row>
   </div>
 </template>
 
@@ -42,9 +46,24 @@ export default {
   name: "home",
   data() {
     return {
-      search_v : '',
-      tabBtnList:['推荐','居家生活','服饰鞋包','美食酒水','个护清洁','母婴亲子','运动旅行','数码家电','严选全球']
-
+      search_v: "",
+      tabBtnList: [
+        "推荐",
+        "居家生活",
+        "服饰鞋包",
+        "美食酒水",
+        "个护清洁",
+        "母婴亲子",
+        "运动旅行",
+        "数码家电",
+        "严选全球",
+      ],
+      imgObj: [
+        "https://yanxuan.nosdn.127.net/bc462b22ff836612ff9aa7f10090ee3d.jpg?type=webp&imageView&quality=75&thumbnail=750x0",
+        "https://yanxuan.nosdn.127.net/1349d7a9a81f90b39578f0b69d39bac7.jpg?type=webp&imageView&quality=75&thumbnail=750x0",
+        "https://yanxuan.nosdn.127.net/3d61a8db02d1aa7ef0b4da1b7b6a8fdd.png?type=webp&imageView&quality=75&thumbnail=750x0",
+        "https://yanxuan.nosdn.127.net/976b254bb78e067bf5830153b28b8502.jpg?type=webp&imageView&quality=75&thumbnail=750x0",
+      ],
     };
   },
 };
@@ -52,7 +71,7 @@ export default {
 
 <style>
 /* header */
-.header-div{
+.header-div {
   display: flex;
   flex-wrap: nowrap;
   flex-direction: row;
@@ -79,9 +98,14 @@ export default {
   height: 1rem;
   color: red;
   border: 1px solid red;
-  font-size: .8rem;
+  font-size: 0.8rem;
   line-height: 1rem;
   border-radius: 0.15rem;
   text-align: center;
+}
+
+/* 轮播图 */
+.myswipe {
+  width: 100%;
 }
 </style>
